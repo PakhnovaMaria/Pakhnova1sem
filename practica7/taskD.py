68da2c9119033647885813535aed7e959011613b
Pakhnova <pakhnova97@mail.ru>
2015-10-13 11:47:10 +0300

    решена задача 5

--- practica7/taskD.py
+++ practica7/taskD.py
@@ -1,1 +1,8 @@
-eval("input()")
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.arange(-20, 25.01, 0.1)
+#plt.xkcd()
+formula_string = input("Введите формулу:")
+b = eval(formula_string)
+plt.plot(x, b)
+plt.show()
