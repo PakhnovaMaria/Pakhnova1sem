f075b58b9a41a76bfca945fce454242a8e543f8c
Pakhnova <pakhnova97@mail.ru>
2015-11-03 10:41:35 +0300

    Coding Style -- исправленная версия

---
+++ praktika10/task3.py
@@ -0,0 +1,10 @@
+I = open('input.txt', 'r')
+words = I.read()
+words = words.replace('.',' ').replace(',',' ').replace('?',' ').replace('!',' ')
+words = words.lower()
+n = 1
+for x in words:
+    if
+
+print(words)
+I.close()
