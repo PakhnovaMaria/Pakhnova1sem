4b7a2f2b91e748613b6232500fbce069d944c593
Maria <pakhnova97@mail.ru>
2015-11-30 19:35:44 +0300

    Промежуточный коммит

--- practica13/matrix.py
+++ practica13/matrix.py
@@ -1,7 +1,10 @@
 class Matrix:
-    def __init__(self, m, n=None):#m - РєРѕР»РІРѕ СЃС‚СЂРѕРє, n- РєРѕР»РІРѕ СЃС‚РѕР»Р±С†РѕРІ
-        if not isinstance(m, int):
+    def __init__(self, m, n=None):# m - колво строк, n- колво столбцов
+        if not (isinstance(m, int) or isinstance(m, list)):
             raise ValueError()
+        if n != None:
+            if not(isinstance(n, int) or isinstance(n, list)):
+                raise ValueError()
         if m <= 0:
             raise ValueError()
         if n == None:
@@ -9,11 +12,12 @@ class Matrix:
             self.Matr = m
             self.m = len(M)
             self.n = len(M[0])
+            if not isinstance(m, list): raise ValueError()
         if type(m) == int and type(n) == int:
             if n > 0 and m > 0:
                 self.m = m
                 self.n = n
-                self. Matr = [[0] * n for i in range(m)]
+                self.Matr = [[0] * n for i in range(m)]
 
     def __add__(self, other):
         if self.m == other.m and self.n == other.n:
@@ -49,19 +53,26 @@ class Matrix:
     def set(self, i, j, value):
         self.Matr[i][j] = value
 
+    def get_size(self):
+        A = (self.m, self.n)
+        return A
+
+    def transpose(self):
+        if self.m == self.n:
+            for i in range(self.m):
+                for j in range(self.n):
+                    if i < j:
+                        self.Matr[i][j], self.Matr[j][i] = self.Matr[j][i], self.Matr[i][j]
+                        return self.Matr
+
     def __mul__(self, other):
-        if type(other)  == int:
+        if type(other) == int:
              for i in range(self.m):
                 for j in range(self.n):
                     self.Matr[i][j] = self.Matr[i][j] * other
                     return self.Matr
-             else:
-                 if self.n == other.m:
-                     for i in range(self.m):
-                         for j in range(other.n):
-                             self.Matr[i][j] = self.i*other.j
-
-
+    def determinant(self):
+        pass
 
 
 
