253e7de8a7f2e6f30a6970648ad91d282e128929
PakhnovaMaria <PakhnovaMaria@users.noreply.github.com>
2016-02-14 20:19:46 +0300

    исправленная версия

--- practica16/task2.py
+++ practica16/task2.py
@@ -1,48 +1,50 @@
 import argparse
 import sys
 
-parser = argparse.ArguementParser(description='Consolnyi calculator')
+parser = argparse.ArgumentParser(description='Consolnyi calculator')
 
-parser.add_arguement('values', metavar='Values', type=float, nargs='+', help='входные данные')
+parser.add_argument('values', metavar='Values', type=float, nargs='+', help='Dannie')
+parser.add_argument('-a', '--action', action="store", help='vupolnit operaziu')
 
-parser.add_arguement('-a', '--action', Action='store_true', help='выполнить арифметическую операцию')
-parser.add_arguement('-v', '--verbose', Action='store_true', help='вывести вычисляемое выражение со знаком равенства перед ответом')
+parser.add_argument('-v', '--verbose', action="store", help='vuvesti vuragenie')
 
 args = parser.parse_args()
 
-if not args.action and not args.verbos:
-    print('Должен быть указан хотя бы один из параметров --action и --verbose', file=sys.strderr)
+if not args.action and not args.verbose:
+    print('ukazan odin iz parametrov --action i --verbose', file=sys.stderr)
     sys.exit(-1)
 
 x=float(args.values[0])
 y=float(args.values[1])
 
 if args.action:
-    if args.action == '+':
-        print(x+y)
-    elif args.action == '-':
-        print(x-y)
-    elif args.action == '*':
-        print(x*y)
-    elif args.action == '/':
-        print(x/y)
-else:
-    print('Неверный знак операции', file=sys.strderr)
-    sys.exit(-1)
+    if args.action == '+' or '-' or '*' or '/':
+        if args.action == '+':
+            print(x+y)
+        elif args.action == '-':
+            print(x-y)
+        elif args.action == '*':
+            print(x*y)
+        elif args.action == '/':
+            print(x/y)
+    else:
+        print('nevernui znak operazii', file=sys.stderr)
+        sys.exit(-1)
 
 if args.verbose:
-    if args.verbose == '+':
-        z = float(x+y)
-        print(x, args.verbose, y, '=', z)
-    elif args.action == '-':
-        z = float(x-y)
-        print(x, args.verbose, y, '=', z)
-    elif args.action == '*':
-        z = float(x*y)
-        print(x, args.verbose, y, '=', z)
-    elif args.action == '/':
-        z = float(x/y)
-        print(x, args.verbose, y, '=', z)
-else:
-    print('Неверный знак операции', file=sys.strderr)
-    sys.exit(-1)
+    if args.action == '+' or '-' or '*' or '/':
+        if args.verbose == '+':
+            z = x+y
+            print(x, args.verbose, y, '=', z)
+        elif args.verbose == '-':
+            z = x-y
+            print(x, args.verbose, y, '=', z)
+        elif args.verbose == '*':
+            z = x*y
+            print(x, args.verbose, y, '=', z)
+        elif args.verbose == '/':
+            z = x/y
+            print(x, args.verbose, y, '=', z)
+    else:
+        print('nevernui znak operazii', file=sys.stderr)
+        sys.exit(-1)
